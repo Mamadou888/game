d38d215c12f6338e0db24b6a2e198c6a5b4ba283
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2013-02-02 20:23:41 -0500

    final day1

--- game.rb
+++ game.rb
@@ -1,12 +1,46 @@
-random_number = rand(100) + 1
-begin
-puts "Guess a number between 1 and 100"
+puts "Pick the highest possible guessing number."
+puts
+range_limit = gets.chomp.to_i
+puts
+puts "How many tries would you like."
+puts
+max_tries = gets.chomp.to_i
+random_number = rand(range_limit) + 1
+guess = 0
+tries = 0
+def message(level) 
+	puts "--------Too " + "#{level} !------------"
+end
+puts
+	sleep 1.0 
+	puts "..."
+	sleep 1.0 
+puts
+puts "let's get started"
+until guess == random_number || tries == max_tries
+puts
+puts "----------------------------------------------"
+puts "Guess a number between 1 and #{range_limit}"
+puts 
 guess = gets.chomp.to_i
 if guess > random_number
-	puts "TOO high!"
+	message("high")
 elsif guess < random_number
-	puts "Too low!"
-else 
-puts "Your awesome. You got the correct number."
+	message("low")
+elsif guess == random_number	
+puts "*****Your awesome. You got the correct number.*****"
+end
+puts
+tries += 1 
+until_limit = max_tries - tries
+puts "You have #{until_limit} out of #{max_tries} tries left."
+if tries == max_tries
+	puts 
+	#making loosing fun and embarrassing.
+	5.times do 
+	puts "Game over!!!!"
+	puts 
+	end
+	puts "Too much guessing. Come on... you are better than that."
+end
 end 
-end until guess == random_number
